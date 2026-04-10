a9dffe70dca584ae544c1b8a5f008823f815eb94
ali-akbar-dev <aliakbareng09@gmail.com>
2026-04-10 21:17:17 +0600

    Add files via upload

---
+++ day3_mini_calculator.py
@@ -0,0 +1,19 @@
+#mini_calculator 10 + 20 = 30
+first = input ("Enter 1st Number : ")
+operator = input ("Enter A Operator : +,-,*,/")
+second = input ("Enter 2nd Number : ")
+
+first = int (first)
+second = int (second)
+if operator == "+" :
+    print (first + second)
+elif operator == "-" : 
+    print (first - second)
+elif operator =="*" :
+        print (first * second)
+elif operator =="/" :
+            print (first / second)
+elif operator =="%" : 
+            print (first % second)
+else :
+               print ("Operator Invalidated")
